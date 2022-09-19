e8e383f7a9d131b973603f6c8d252f8dd6a95547
Oscar Espana <espaaoscar@gmail.com>
2022-09-18 22:46:59 -0500

    feat: add query and resolver to get a job

--- src/resolvers/job.ts
+++ src/resolvers/job.ts
@@ -1,7 +1,13 @@
 import { Job } from "../database";
 
+interface Job {
+  title: string;
+  description: string;
+}
+
 export const resolvers = {
   Query: {
     jobs: () => Job.findAll(),
+    job: (_root: any, args: { id: string }) => Job.findById(args.id),
   },
 };
--- src/typedefs/job.ts
+++ src/typedefs/job.ts
@@ -2,12 +2,13 @@ import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
   type Job {
-    id: String
+    id: ID!
     title: String
     description: String
   }
 
   type Query {
     jobs: [Job]
+    job(id: String!): Job
   }
 `;
